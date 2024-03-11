7d4aa332e44be2e477f52045b2f1166cbe7ac340
Kushal Shrestha <shresthakushal61@gmail.com>
2024-03-11 19:57:28 +0545

    Components, Props & JSX

--- 1-components-props-jsx/src/App.jsx
+++ 1-components-props-jsx/src/App.jsx
@@ -5,18 +5,18 @@ export default function App() {
   
   let obj1={
     isrc:`${thumbnail}3`,
-    title:'Node.js Full Course',
-    // channel:'Kushal Code',
-    views:'200K',
-    time:'6 months ago'
+    title:'Daily Practices for Inner Peace',
+    // channel:'SerenitySphere',
+    views:'1M',
+    time:'2 years ago'
   };
 
   let obj2={
     isrc:`${thumbnail}4`,
-    title:'MongoDB Full Course',
-    channel:'Kushal Code',
-    views:'150K',
-    time:'3 months ago'
+    title:'The Future of Virtual Reality',
+    channel:'FutureTechInsights',
+    views:'800K',
+    time:'6 months ago'
   };
 
   return (
@@ -26,16 +26,16 @@ export default function App() {
         <div className="allVideos">
           <Video
             isrc={`${thumbnail}1`}
-            title="React.js Full Course"
-            channel="Kushal Code"
+            title="Journey Through Ancient Ruins"
+            channel="AdventureSeekersTV"
             views="100K"
             time="1 year ago"
           />
           <Video
             isrc={`${thumbnail}2`}
-            title="Redux Full Course"
-            channel="Kushal Code"
-            views="50K"
+            title="East Meets West Recipes"
+            channel="CulinaryCrossroads"
+            views="250K"
             time="8 months ago"
           />
           <Video {...obj1}/>
