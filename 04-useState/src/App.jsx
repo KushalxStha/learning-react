47500e41f1eb58719cc6f842a52d409f9695aa59
Kushal Shrestha <shresthakushal61@gmail.com>
2024-04-08 09:49:51 +0545

    useState hook

---
+++ 04-useState/src/App.jsx
@@ -0,0 +1,13 @@
+// import Counter from "./components/Counter"
+import BgChanger from "./components/BgChanger"
+
+function App() {
+  return (
+    <>
+      {/* <Counter></Counter> */}
+      <BgChanger></BgChanger>
+    </>
+  )
+}
+
+export default App
